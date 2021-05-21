d2bb48bd396ca4d3ecbd68cdd32deafef8666c34
ddgryaz <dzuman1996@mail.ru>
2021-05-21 13:27:08 +0500

    mybrains in myprofile - display flex

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -83,7 +83,6 @@ const Profile = observer(() => {
                             border: '1px solid black',
                             display: "flex",
                         }}>
-                            
                         </div>
                     </div>
                 </div>
