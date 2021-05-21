1a6d570643f715c2823f92e43d22fbfcaba708af
ddgryaz <dzuman1996@mail.ru>
2021-05-21 21:25:10 +0500

    Брейны в профиле кликабельны

--- frontend/src/components/MyBrainItem.js
+++ frontend/src/components/MyBrainItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Image from "react-bootstrap/Image";
+import {useHistory} from "react-router-dom";
+import {BRAIN_ROUTE} from "../utils/consts";
 
 const MyBrainItem = ({brain}) => {
+    const history = useHistory()
     return (
-        <div style={{width: 50, height: 50, margin: 20}}>
+        <div style={{width: 50, height: 50, margin: 20, cursor: 'pointer'}}
+             onClick={() => history.push(BRAIN_ROUTE + '/' + brain.id)}>
             <Image width={50} height={50} src={process.env.REACT_APP_API_URL + brain.img}/>
         </div>
     );
