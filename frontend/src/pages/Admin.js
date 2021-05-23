613160e8dea90383fe16f55f906540a8d712e68a
ddgryaz <dzuman1996@mail.ru>
2021-05-23 13:09:15 +0500

    добавлен функционал создания администратора из админки

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import {Button, Container} from "react-bootstrap";
 import CreateBrain from "../components/modals/CreateBrain";
+import CreateUser from "../components/modals/CreateUser";
 
 const Admin = () => {
     const [brainVisible, setBrainVisible] = useState(false)
+    const [userVisible, setUserVisible] = useState(false)
     return (
         <Container className="d-flex flex-column">
             <Button variant={"success"} className='mt-2' onClick={() => setBrainVisible(true)}>Добавить Брейн</Button>
-            <Button variant={"success"} className='mt-2'>Добавить Пользователя || Администратора</Button>
+            <Button variant={"success"} className='mt-2' onClick={() => setUserVisible(true)}>Добавить Пользователя || Администратора</Button>
             <CreateBrain show={brainVisible} onHide={() => setBrainVisible(false)}/>
+            <CreateUser show={userVisible} onHide={() => setUserVisible(false)}/>
         </Container>
     );
 };
