99f5c7c8db5599646e9576dbc378c590b20eded4
ddgryaz <dzuman1996@mail.ru>
2021-07-10 01:27:16 +0500

    [FIX] => Вернулась навигация в статистике

--- frontend/src/pages/PopularityBrains.js
+++ frontend/src/pages/PopularityBrains.js
@@ -2,6 +2,10 @@ import React, {useEffect, useState} from 'react';
 import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack} from 'victory';
 import {observer} from "mobx-react-lite";
 import {mostPopularBrains} from "../http/mostPopularAPI";
+import {Col, Row} from "react-bootstrap";
+import Navigation from "../components/Navigation";
+import BrainsList from "../components/BrainsList";
+import Pages from "../components/Pages";
 
 const PopularityBrains = observer(() => {
     // const items = [
@@ -17,30 +21,39 @@ const PopularityBrains = observer(() => {
         mostPopularBrains().then(data => setBrains(data))
     }, [])
     return (
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: '5%'}}>
-            <div style={{width: '70vw', height: '70vh', backgroundColor: 'white'}}>
-                <VictoryChart
-                    domainPadding={20}
-                    theme={VictoryTheme.grayscale}
-                >
-                    <VictoryAxis
+        <Row className="mt-2">
+            <Col md={3} className="mt-3">
+                <Navigation/>
 
-                    />
-                    <VictoryAxis
-                        dependentAxis
-                    />
-                    <VictoryStack
-                        colorScale={"warm"}
-                    >
-                        <VictoryBar
-                            data={brains}
-                            x={"name"}
-                            y={"popularity"}
-                        />
-                    </VictoryStack>
-                </VictoryChart>
-            </div>
-        </div>
+            </Col>
+            <Col md={9}>
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: '5%'}}>
+                    <div style={{width: '70vw', height: '70vh', backgroundColor: 'white'}}>
+                        <VictoryChart
+                            domainPadding={20}
+                            theme={VictoryTheme.grayscale}
+                        >
+                            <VictoryAxis
+
+                            />
+                            <VictoryAxis
+                                dependentAxis
+                            />
+                            <VictoryStack
+                                colorScale={"warm"}
+                            >
+                                <VictoryBar
+                                    data={brains}
+                                    x={"name"}
+                                    y={"popularity"}
+                                />
+                            </VictoryStack>
+                        </VictoryChart>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+
     );
 })
 
